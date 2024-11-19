8b95afc1dda9e2943270e8dcb8603230bae3762c
mnt <c0b230879e@edu.teu.ac.jp>
2024-11-19 16:12:42 +0900

    ループ解消

--- pygame_image.py
+++ pygame_image.py
@@ -13,18 +13,27 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     tmr = 0
     koukaton = pg.image.load("fig/3.png")
+    bg_img_gyaku = pg.image.load("fig/pg_bg.jpg")
+    bg_img_gyaku = pg.transform.flip(bg_img_gyaku,True,False)
     koukaton = pg.transform.flip(koukaton,True,False)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        x = tmr%3200
 
-        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img_gyaku,[-x+1600,0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img_gyaku,[-x+4800,0])
         screen.blit(koukaton,[300,200])
         
         pg.display.update()
-        if tmr == 800:
-            tmr ==0
-        tmr += 1        
+        
+        tmr += 1  
+        # if tmr == 800:
+        #     screen.blit(bg_img_gyaku, [-tmr, 0])
+        #     tmr =0      
+        
         clock.tick(200)
 
 
