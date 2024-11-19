8ff76cd96750db7e8b8bd89b49f0ef7befc0add9
mnt <c0b230879e@edu.teu.ac.jp>
2024-11-19 16:32:23 +0900

    左右移動可能

--- pygame_image.py
+++ pygame_image.py
@@ -4,36 +4,43 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
-    pg.fps = 200
-    clock  = pg.time.Clock()
+    clock = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    tmr = 0
+    bg_img_gyaku = pg.transform.flip(bg_img, True, False)  # 背景を反転
     koukaton = pg.image.load("fig/3.png")
-    bg_img_gyaku = pg.image.load("fig/pg_bg.jpg")
-    bg_img_gyaku = pg.transform.flip(bg_img_gyaku,True,False)
-    koukaton = pg.transform.flip(koukaton,True,False)
+    koukaton = pg.transform.flip(koukaton, True, False)  # こうかとんを反転
+    koukaton_rct = koukaton.get_rect()
+    koukaton_rct.center = (300, 200)
+
+    tmr = 0
+
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
-        x = tmr%3200
+            if event.type == pg.QUIT:
+                return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            koukaton_rct.move_ip(0,-1)
+        if key_lst[pg.K_DOWN]:
+            koukaton_rct.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:
+            koukaton_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            koukaton_rct.move_ip(1, 0)
 
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img_gyaku,[-x+1600,0])
-        screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(bg_img_gyaku,[-x+4800,0])
-        screen.blit(koukaton,[300,200])
-        
+        screen.blit(bg_img_gyaku, [-x + 1600, 0])
+        screen.blit(bg_img, [-x + 3200, 0])
+
+        screen.blit(koukaton, koukaton_rct)
+
         pg.display.update()
-        
-        tmr += 1  
-        # if tmr == 800:
-        #     screen.blit(bg_img_gyaku, [-tmr, 0])
-        #     tmr =0      
-        
+
+        tmr += 1
         clock.tick(200)
 
 
